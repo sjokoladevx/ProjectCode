2decfbe3d8408cf1975273496410f72b319c182e
sjokoladevx <stephen.i.prager.14@dartmouth.edu>
2014-03-04 18:49:18 -0500

    hihi

--- libcflie_delivery/src/control/control.cpp
+++ libcflie_delivery/src/control/control.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2013, Jan Winkler <winkler@cs.uni-bremen.de>
+// HIHIHIH Copyright (c) 2013, Jan Winkler <winkler@cs.uni-bremen.de>
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
